392f5cdb0db30028c2c315edeee537a710bc1f33
Madeeha Tassadaq <madeehatassadaq@outlook.com>
2024-04-18 20:06:30 +0500

    statistics

---
+++ BMI_app.py
@@ -0,0 +1,7 @@
+weight=input('enter your weight here\n')
+height=input('enter your height here\n')
+weight=float(weight)
+height=float(height)
+BMI=weight/height**2
+name=input('enter your name here\n')
+print("your name is",name,'and my BMI is',BMI)
